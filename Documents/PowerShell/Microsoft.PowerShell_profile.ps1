881c2c2cb4e7937090e8ac4556df0d8b8a47dc19
mateusdigital <hello@mateus.digital>
2024-03-31 18:34:13 -0300

    Make the clone repos to separated folders...

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -314,28 +314,63 @@ function ds() { dots s };
 function dp() { dots p };
 function dg() { dots gui };
 
-
 ## -----------------------------------------------------------------------------
 function download-my-git-repos() {
     $user_name = "mateusdigital";
-    $api_url   = "https://api.github.com/users/$user_name/repos"
+    $api_url   = "https://api.github.com/users/mateusdigital/repos?per_page=100"
 
     $projects_dir = "$HOME/Projects/$user_name";
 
-    $response   = Invoke-RestMethod -Uri $api_url
-    $repo_names = $response | ForEach-Object { $_.name }
+    $response = Invoke-RestMethod -Uri $api_url
 
     # Output the repository names
-    foreach($name in $repo_names) {
-        $target_dir = "$projects_dir/$name";
+    foreach($item in $response) {
+        $name   = $item.name
+        $topics = $item.topics;
+        $type   = "other";
+
+        if(($topics -contains "demo")) {
+            $type = "demo";
+        }
+        if(($topics -contains "game")) {
+            $type = "game";
+        }
+        if(($topics -contains "library")) {
+            $type = "library";
+        }
+        if(($topics -contains "tool")) {
+            $type = "tool";
+        }
+        if(($topics -contains "personal")) {
+            $type = "personal";
+        }
+        if(($topics -contains "website")) {
+            $type = "website";
+        }
+
+        $target_dir = "$projects_dir/$type/$name";
         if(Test-Path "$target_dir") {
             Write-Output "($name) is already clonned...";
             continue;
         }
 
+        $base_dir = "$projects_dir/$type";
+        if(-not (Test-Path "$base_dir")) {
+            New-Item -ItemType Directory -Path  "$base_dir";
+        }
+
         Write-Output "Downloading: ($name) to ${projects_dir}";
         git clone "git@github.com:mateusdigital/$name" "$target_dir";
     }
+}
+
+
+## -----------------------------------------------------------------------------
+function clean-temp-repos()
+{
+    $temp_dir = "$HOME/Projects/_temp";
+    Remove-Item -Force -Recurse "$temp_dir";
+    New-Item -ItemType Directory -Path "$temp_dir";
 }
 
 ##
@@ -485,4 +520,4 @@ function touch_all_files() {
 }
 
 ## -----------------------------------------------------------------------------
-Import-VisualStudioEnvironment
+# Import-VisualStudioEnvironment

cfe7dfe6f9591aadcdc6f216eab220072fd8088f
mateusdigital <hello@mateus.digital>
2024-04-29 15:10:42 -0300

    Add the yt-dl mp3.

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -515,6 +515,11 @@ function yt()
     yt-dlp.exe $args;
 }
 
+## -----------------------------------------------------------------------------
+function yt-mp3()
+{
+    yt-dlp.exe --extract-audio $args
+}
 
 ##
 ## Videos

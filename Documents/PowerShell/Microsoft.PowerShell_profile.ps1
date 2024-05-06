529f263294edc546657e12c7fe545f390d35e770
mateusdigital <hello@mateus.digital>
2024-05-06 07:15:07 -0300

    Add license header.

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,24 @@
-. "$HOME/.config/powershell/Microsoft.PowerShell_profile.ps1"
+##----------------------------------------------------------------------------##
+##                               *       +                                    ##
+##                         '                  |                               ##
+##                     ()    .-.,="``"=.    - o -                             ##
+##                           '=/_       \     |                               ##
+##                        *   |  '=._    |                                    ##
+##                             \     `=./`,        '                          ##
+##                          .   '=.__.=' `='      *                           ##
+##                 +                         +                                ##
+##                      O      *        '       .                             ##
+##                                                                            ##
+##  File      : Microsoft.PowerShell_profile.ps1                              ##
+##  Project   : PowerShell                                                    ##
+##  Date      : 2024-04-29                                                    ##
+##  License   : See project's COPYING.TXT for full info.                      ##
+##  Author    : mateus.digital <hello@mateus.digital>                         ##
+##  Copyright : mateus.digital - 2024                                         ##
+##                                                                            ##
+##  Description :                                                             ##
+##                                                                            ##
+##----------------------------------------------------------------------------##
+
+## Source the actual file :)
+. "$HOME/.config/powershell/Microsoft.PowerShell_profile.ps1";

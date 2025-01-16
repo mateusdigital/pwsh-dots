507f56c06edc96f85628825f936533754ab47eb2
mateusdigital <hello@mateus.digital>
2025-01-16 14:58:05 -0300

    clean up old stuff

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -427,56 +427,6 @@ function dots()
       $args;
 }
 
-## -----------------------------------------------------------------------------
-function download-my-git-repos()
-{
-  $user_name = "mateusdigital";
-  $api_url   = "https://api.github.com/users/${user_name}/repos?per_page=100"
-
-  $projects_dir = "$HOME/Projects/${user_name}";
-
-  $response = Invoke-RestMethod -Uri $api_url;
-
-  # Output the repository names
-  foreach($item in $response) {
-    $name   = $item.name
-    $topics = $item.topics;
-    $type   = "other";
-
-    if(($topics -contains "demo")) {
-      $type = "demo";
-    }
-    if(($topics -contains "game")) {
-      $type = "game";
-    }
-    if(($topics -contains "library")) {
-      $type = "library";
-    }
-    if(($topics -contains "tool")) {
-      $type = "tool";
-    }
-    if(($topics -contains "personal")) {
-      $type = "personal";
-    }
-    if(($topics -contains "website")) {
-      $type = "website";
-    }
-
-    $target_dir = "$projects_dir/$type/$name";
-    if(Test-Path "$target_dir") {
-      Write-Output "($name) is already clonned...";
-      continue;
-    }
-
-    $base_dir = "$projects_dir/$type";
-    if(-not (Test-Path "$base_dir")) {
-      New-Item -ItemType Directory -Path  "$base_dir";
-    }
-
-    Write-Output "Downloading: ($name) to ${projects_dir}";
-    git clone "git@github.com:${user_name}/${name}" "$target_dir";
-  }
-}
 
 ## -----------------------------------------------------------------------------
 function repochecker-all()
@@ -714,45 +664,15 @@ function touch_all_files()
 }
 
 
-##
-## Vim
-##
-
-## -----------------------------------------------------------------------------
-function neovim() { nvim.exe $args }
-function vim()    { nvim.exe $args }
-function vi()     { nvim.exe $args }
-
-## -----------------------------------------------------------------------------
-function v()
-{
-  if($args.Count -eq 0) {
-    nvim.exe $PWD;
-    return;
-  }
-
-  nvim.exe $args;
-}
-
-
-## -----------------------------------------------------------------------------
-function vf()
-{
-  if($args.Count -eq 0) {
-      vifm.exe $PWD;
-      return;
-  }
-
-  vifm.exe $args;
-}
-
 
 ##
 ## Powerline
 ##
 
 ## -----------------------------------------------------------------------------
-Import-Module PSReadLine;
+if (-not (Get-Module -Name PSReadLine)) {
+  Import-Module PSReadLine;
+}
 
 
 ## -----------------------------------------------------------------------------
